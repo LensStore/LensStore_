6a091d2990880a9078fe07034c7720f3b2120924
LensStore <andriandwisaputra71@gmail.com>
2026-02-05 00:28:04 +0700

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,23 @@
+
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../views'))
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'BiolinkHub',
+        description: 'Vercel Ready'
+    })
+})
+
+export default app
